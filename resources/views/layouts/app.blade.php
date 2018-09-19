edb410f68b0660f098b0440e9df810b49388d73a
vvmsecured <vvmspace@gmail.com>
2018-09-19 13:23:54 +0500

    YANDEX_VERIFICATION

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    {{ env('YANDEX_VERIFICATION', '') }}
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
