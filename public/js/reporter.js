4f1b02e92c7b3b29c7ac5d6768da65a748c83870
vvmsecured <vvmspace@gmail.com>
2018-09-21 15:37:03 +0500

    Roles

--- public/js/reporter.js
+++ public/js/reporter.js
@@ -4,6 +4,7 @@ $(function(){
         let id = $(this).data('id');
         $.getJSON('/report/error/' + id, function (station) {
             alert(station.name + ' stream error reported. Thank you. It is better and it will be better.');
+            document.location.reload();
         });
     });
 });
