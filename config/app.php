1affb191fe8c209838c334455a6684775c562295
vvmsecured <vvmspace@gmail.com>
2018-10-17 00:27:35 +0500

    app name: listen radio online

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Radio.VVM.SPACE'),
+    'name' => env('APP_NAME', 'Listen Radio Online'),
 
     /*
     |--------------------------------------------------------------------------
