f4b9e741a2d04ab18d77353cac38a11d821deaa2
vvmsecured <vvmspace@gmail.com>
2018-10-17 02:15:51 +0500

    - alert('error')

--- public/js/reporter.js
+++ public/js/reporter.js
@@ -1,6 +1,5 @@
 $(function(){
     $('audio').on('error', function(){
-        alert('error');
         let id = $(this).data('id');
         $.getJSON('/report/error/' + id, function (station) {
             alert(station.name + ' stream error reported. Thank you. It is better and it will be better.');
