63728f4243a2f868deeb1fef56b72c1ba71879a9
Vladimir V. Myagdeev <vvmspace@gmail.com>
2018-11-28 14:11:14 +0500

    listened

--- resources/views/stations/index.blade.php
+++ resources/views/stations/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('title', $title ?? config('app.name', 'Laravel'))
 @section('content')
-    <div class="container">
+    <div class="container stations-list">
         <div class="row">
             @if(!empty($h1))
             <div class="col-md-12">
@@ -9,8 +9,8 @@
             </div>
             @endif
         @foreach($stations as $station)
-            <div class="col-md-4">@php $stream = $station->best_stream(); @endphp
-                <div><b><a href="/{{ $station->slug }}-online">{{ $station->name  }}</a></b> {{ $station->country->name }}</div>
+            <div class="col-md-4 stations-list-item">@php $stream = $station->best_stream(); @endphp
+                <div><b><a href="/{{ $station->slug }}-online">{{ $station->name  }}</a></b> {{ $station->country->name }}<span class="stations-list-item-listened"> {{ $station->listened ?? 0 }}</span> </div>
                 <div><audio data-id="{{ $stream->id }}" data-station="{{ $station->id }}" style="width: 100%;" src="{{ $stream->getUrl() }}" controls preload="none"></audio></div>
             </div>
         @endforeach
